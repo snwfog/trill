ef7f495522d82cc48b18541ca2887b5d80ba5c81
Charles Yang <cyang@cloudops.com>
2014-06-30 21:33:50 -0400

    Got some weird js files

--- GruntFile.js
+++ GruntFile.js
@@ -1,67 +1,63 @@
 module.exports = function(grunt) {
 
-    grunt.loadNpmTasks('grunt-bower-task')
-    grunt.loadNpmTasks('grunt-contrib-watch')
-    grunt.loadNpmTasks('grunt-contrib-clean')
-    grunt.loadNpmTasks('grunt-contrib-copy')
-    grunt.loadNpmTasks('grunt-contrib-concat')
-    grunt.loadNpmTasks('grunt-open')
+  grunt.loadNpmTasks('grunt-bower-task')
+  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-contrib-clean')
+  grunt.loadNpmTasks('grunt-contrib-copy')
+  grunt.loadNpmTasks('grunt-contrib-concat')
+  grunt.loadNpmTasks('grunt-open')
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-        concat: {
-            client: {
-                src: ["src/game/**/*.js"],
-                dest: 'deploy/<%= pkg.name %>.js'
-            }
-        },
+    concat: {
+      client: {
+        src: ["src/game/**/*.js"],
+        dest: 'deploy/<%= pkg.name %>.js'
+      }
+    },
 
-        watch: {
-            files: 'src/**/*.js',
-            tasks: ['compile-and-concat']
-        },
+    watch: {
+      files: 'src/**/*.js',
+      tasks: ['compile-and-concat']
+    },
 
-        open: {
-            dev: {
-                path: 'http://localhost:8080/trill.html'
-            }
-        },
+    open: {
+      dev: {
+        path: 'http://localhost:8080/trill.html'
+      }
+    },
 
-        copy: {
-            client: {
-                files: [
-                    {
-                        expand: true,
-                        flatten: true,
-                        src: ['./src/static/**/*.html', './src/static/**/*.css', './src/static/**/*.png', './src/game/trill-server.js', './src/game/server/server.js'],
-                        dest: './deploy'
-                    }
-                ]
-            },
-            server: {
-                files: [
-                    {
-                        expand: true,
-                        flatten: true,
-                        src: ['src/server/**/*.js'],
-                        dest: 'deploy/server'
-                    }
-                ]
-            }
-        },
-        
-        bower: {
-            install: {
-                options: {
-                    verbose: true
-                }
-            }
-        },
-        clean: ["./deploy", './lib']
-    })
+    copy: {
+      client: {
+        files: [{
+          expand: true,
+          flatten: true,
+          src: ['./src/static/**/*.html', './src/static/**/*.css', './src/static/**/*.png', './src/game/trill-server.js', './src/game/server/server.js'],
+          dest: './deploy'
+        }]
+      },
+      server: {
+        files: [{
+          expand: true,
+          flatten: true,
+          src: ['src/server/**/*.js'],
+          dest: 'deploy/server'
+        }]
+      }
+    },
 
-    grunt.registerTask('default', ['clean', 'bower:install', 'compile-and-concat'])
-    grunt.registerTask('open-game', ['default', 'open'])
-    grunt.registerTask('compile-and-concat', ['concat', 'copy'])
+    bower: {
+      install: {
+        options: {
+          verbose: true
+        }
+      }
+    },
+    clean: ["./deploy", './lib']
+  })
+
+  grunt.registerTask('default', ['clean', 'bower:install', 'compile-and-concat'])
+  grunt.registerTask('open-game', ['default', 'open'])
+  grunt.registerTask('compile-and-concat', ['concat', 'copy'])
 }
