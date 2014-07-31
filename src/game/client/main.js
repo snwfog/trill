6a116f6325a523e62fb5fde0ca192970a17ea8e9
Raza Robson <robson@nexonia.com>
2014-07-31 11:48:24 -0400

    added window resize event callback.

--- src/game/client/main.js
+++ src/game/client/main.js
@@ -1,10 +1,37 @@
 var webapi = require('./webapi.js');
 var ingamestate = require('./inGameState.js');
 
-var game = new Phaser.Game(760, 1024, Phaser.AUTO, 'game', null, false, false);
+
+var maxDimensions = {
+  w:2048,
+  h:1536
+};
+
+var initDimensions = {
+  w:Math.min(window.innerWidth, maxDimensions.w),
+  h:Math.min(window.innerHeight, maxDimensions.h)
+};
+
+var game = new Phaser.Game(initDimensions.w, initDimensions.h, Phaser.AUTO, 'game', null, false, false);
 
 game['webapi'] = new webapi({
-  url: 'http://localhost:8080/'
+  url: 'https://tlyivpvrap.localtunnel.me'
+});
+
+$(window).resize(function(){
+  var width = Math.min (maxDimensions.w, $(window).width());
+  var height = Math.min (maxDimensions.h, $(window).height());
+
+  game.width = width;
+  game.height = height;
+  game.stage.bounds.width = width;
+  game.stage.bounds.height = height;
+
+  if (game.renderType === Phaser.WEBGL)
+  {
+    game.renderer.resize(width, height);
+  }
+
 });
 
 game.state.add('inGameState', new ingamestate(), true);
