e2386ac583519e92f4dacfe2725b45472b93df82
raza robson <robson@nexonia.com>
2015-01-24 17:10:08 -0500

    made leaves go to the back.

--- src/game/client/state/menuState.js
+++ src/game/client/state/menuState.js
@@ -27,6 +27,16 @@ MenuState.prototype.onCreate = function () {
   this.helpButton = factory.button.grey.small(this.game, "?");
   this.popup = factory.popup(this.game, "This is some help text.");
 
+  this.emitter = this.game.make.emitter(0, 0, 100);
+  this.emitter.makeParticles("leaf");
+  this.emitter.gravity = 0;
+  this.emitter.width = this.game.world.width * 1.1;
+  this.emitter.setYSpeed(30, 100);
+  this.emitter.z = 0;
+  this.emitter.start(false);
+
+  this.group.add(this.emitter);
+
   this.addMods([
     this.title,
     this.newButton,
@@ -36,14 +46,6 @@ MenuState.prototype.onCreate = function () {
   ]);
 
   factory.tween.fadeIn(this).start();
-
-  this.emitter = this.game.add.emitter(this.game.world.centerX, -32, 100);
-  this.emitter.makeParticles("leaf");
-  this.emitter.gravity = 0;
-  this.emitter.width = this.game.world.width * 1.1;
-  this.emitter.setYSpeed(30, 100);
-  this.emitter.z = 0;
-  this.emitter.start(false);
 };
 
 MenuState.prototype.onPostCreate = function () {
@@ -68,7 +70,7 @@ MenuState.prototype.onRender = function () {
 
 MenuState.prototype.onResize = function (width, height) {
 
-  this.emitter.position.setTo(this.game.world.centerX, -32);
+  this.emitter.position.setTo(width/2, -32);
   this.emitter.width = this.game.world.width * 1.1;
 
   this.title.group.position.setTo(width / 2, Math.max(0.25 * height, this.title.bounds.height / 2 + buttonSpacing));
